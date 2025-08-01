a0aa9bb9f8dce0ec245d64ea4d0bc6a1e3391525
cjiaxuan <jiaxuan.cheng@sg.panasonic.com>
2025-08-01 13:23:27 +0800

    update to style

--- public/js/config/mediaGeneration-config.js
+++ public/js/config/mediaGeneration-config.js
@@ -1,10 +1,19 @@
 export const text2Img_steps = [
     {
-        type: "prompts",
-        questions: [
+        type: "prompt",
+        question: [
             {
-                en: 'Objects Present',
-                zh: ''
+                en: 'What are the objects present?',
+                zh: '有哪些物品在场？'
+            },
+        ]
+    },
+    {
+        type: "prompt",
+        question: [
+            {
+                en: 'What are the objects present?',
+                zh: '有哪些物品在场？'
             },
         ]
     },
@@ -16,4 +25,12 @@ export const text2Vid_steps = [
 
 export const img2Vid_steps = [
 
+]
+
+export const doc2vid_steps = [
+
+]
+
+export const url2vid_steps = [
+    
 ]
